b0c47daeab9c308711e2777b48d804fc502fafdc
Victor Marin <Victorm9803@gmail.com>
2022-03-01 20:56:39 +0100

    stream notification

--- src/Other/index.js
+++ src/Other/index.js
@@ -1,47 +1,32 @@
 const { Client, Intents, MessageEmbed } = require("discord.js");
 const { token } = require("./config.json");
 const fetch = require("node-fetch");
-const io = require("socket.io-client");
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
-const socket = io({
-  autoConnect: true,
-  value: "https://www.twitch.tv/shylily",
-});
-const hasAnnounced = false;
 
+var hasAnnounced = false;
 
-function getOnline(){
-    return new Promise(something =>{
-        setTimeout(() =>{
-            something =  fetch(`https://www.twitch.tv/shylily`);
-        },60000)
-    })
-
-}
-
-const checkOnline = () => {
-  while (true) {
-   
-      const live = await getOnline();
-      console.log("fetch");
-      if ((await live.text()).includes("isLiveBroadcast") && hasAnnounced === false) {
-          console.log("if live");
-        client.channels.cache
-          .get("698980600001855550")
-          .send({ content: "@everyone", embeds: [twitchEmbed] });
-          console.log("cache");
-        hasAnnounced = true;
-      } else {
-        hasAnnounced = false;
-        console.log("false");
-      }
-    }
-    
-  
-  
+const checkOnline = async () => {
+  setInterval(() => {
+    ping();
+  }, 60000);
+};
+const ping = async () => {
+  let live = await fetch(`https://www.twitch.tv/memelie`);
+  let text = await live.text();
+  if (text.includes("isLiveBroadcast") && hasAnnounced === false) {
+    console.log("if live");
+    client.channels.cache
+      .get("698980600001855550")
+      .send({ content: "everyone", embeds: [twitchEmbed] });
+    hasAnnounced = true;
+    console.log("cache");
+  } else if (!text.includes("isLiveBroadcast")) {
+    hasAnnounced = false;
+    console.log("false");
+  }
 };
 
 const twitchEmbed = new MessageEmbed()
@@ -64,17 +49,6 @@ client.on("interactionCreate", async (interaction) => {
 
   if (interaction.commandName === "ping") {
     await interaction.reply("Pong!");
-  } else if (interaction.commandName === "live") {
-    await interaction.channel
-      .send({ content: "@everyone", embeds: [twitchEmbed] })
-      .then(interaction.reply({ content: "Done!", ephemeral: true }));
-  } else if (interaction.commandName === "check") {
-    let live = await fetch(`https://www.twitch.tv/shylily`);
-    if ((await live.text()).includes("isLiveBroadcast")) {
-      await interaction.reply("Memelie is live!");
-    } else {
-      await interaction.reply("Memelie isn't live!");
-    }
   }
   //{content: "Hello @everyone... I'm streaming on twitch right now. Feel free to join https://www.twitch.tv/memelie"}
 });
